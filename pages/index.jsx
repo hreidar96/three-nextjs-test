c470f6f2d40fb3ccb507e6668162f8fd8ed6e949
Hreiðar Ingi <hreidar220@gmail.com>
2023-01-07 21:10:01 +0000

    Final build

---
+++ pages/index.jsx
@@ -0,0 +1,32 @@
+import css from "../styles/Home.module.css";
+import { Canvas } from "@react-three/fiber";
+import Box from "../components/Box";
+import OrbitControls from "../components/OrbitControls";
+import LightBulb from "../components/LightBulb";
+import Floor from "../components/Floor";
+import Draggable from "../components/Draggable";
+import {Suspense} from "react";
+
+export default function Home() {
+  return (
+    <div className={css.scene}>
+      <Canvas
+        shadows={true}
+        className={css.canvas}
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight color={"white"} intensity={0.2} />
+        <LightBulb position={[0, 3, 0]} />
+        <Draggable>
+        <Suspense fallback={null}>
+            <Box rotateX={3} rotateY={0.2} />
+        </Suspense>
+        </Draggable>
+        <OrbitControls />
+        <Floor position={[0, -1, 0]} />
+      </Canvas>
+    </div>
+  );
+}
